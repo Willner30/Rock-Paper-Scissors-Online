eee222c70c41ee05dcd3724e7f7b9a8685f7942e
Wilfredo Mota <willmdj@hotmail.com>
2023-08-21 18:58:59 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,10 +2,94 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
-const WebSocket = require('ws');
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const { emit } = require('process');
+const io = new Server(server);
+let connectedUsers = {
+  playerOne: "",
+  playerTwo: ""
+};
+let p1Move = "";
+let p2Move = "";
 
-// Create a WebSocket server
-const wss = new WebSocket.Server({ port: 3000 });
+io.on('connection', (socket) => {
+  // Handle user nickname validation
+  socket.on('userNickname', (nickname) => {
+    if (connectedUsers.playerOne !== "" && connectedUsers.playerTwo !== "") {
+      // If maximum users reached, reject the connection
+      socket.emit('connectionRejected', 'Maximum users reached. Try again later.');
+      socket.disconnect(true);
+    } else if (connectedUsers.playerOne === nickname || connectedUsers.playerTwo === nickname) {
+      // If the nickname is already taken, reject the connection
+      socket.emit('connectionRejected', 'Nickname is already taken. Please choose a different one.');
+    } else if (connectedUsers.playerOne == "" ) {
+      // Assign the first user as 'player1'
+      connectedUsers.playerOne = nickname;
+      socket.emit('connectionAccepted', 'You are the Player 1!!');
+      io.emit('userName1', connectedUsers.playerOne);
+      console.log(connectedUsers.playerOne + ' connected');
+      console.log(connectedUsers);
+    } else if (connectedUsers.playerTwo == "") {
+      // Assign the second user as 'player2'
+      connectedUsers.playerTwo = nickname;
+      socket.emit('connectionAccepted', 'You are the Player 2!!');
+      io.emit('userName2', connectedUsers.playerTwo);
+      console.log(connectedUsers.playerTwo + ' connected');
+      console.log(connectedUsers);
+    }
+    // Handle disconnect event
+    socket.on('disconnect', () => {
+      console.log(nickname + ' disconnected');
+      
+      if (connectedUsers.playerOne === nickname) {
+        connectedUsers.playerOne = "";
+      } else if (connectedUsers.playerTwo === nickname) {
+        connectedUsers.playerTwo = "";
+      }
+    });
+  });
+  socket.on('movePlayed', (player1Move, player2Move) => {  //Verifies moves on the server side
+
+    p1Move = player1Move.move;
+    p2Move = player2Move.move;
+
+    console.log(p1Move);
+    io.emit('move1Played', p1Move);
+    console.log(p2Move);
+    io.emit('move2Played', p2Move);
+    function whoWon() {
+      if (connectedUsers.playerOne === "" || connectedUsers.playerTwo === "") {
+        socket.emit('whoWins', 'Not connected');
+      } else if (p1Move === "" || p2Move === "") {
+        console.log(p1Move + " y " + p2Move);
+        socket.emit('whoWins', 'waiting');
+      } else if (p1Move === p2Move) {
+        // Draw or same move
+        io.emit('whoWins', 'draw');
+      } else if (
+        (p1Move === "rock" && p2Move === "scissors") ||
+        (p1Move === "paper" && p2Move === "rock") ||
+        (p1Move === "scissors" && p2Move === "paper")
+        ) {
+          // Player 1 wins
+        io.emit('whoWins', 'player1');
+      } else {
+        // Player 2 wins
+        io.emit('whoWins', 'player2');
+      }
+    }  
+    whoWon();
+
+  });
+  socket.on('restart', (msg) => {
+    if (msg === "go") {
+      io.emit('restarting', msg);
+    }
+  });
+});
+
 
 app.use(express.json());
 app.use(express.static(__dirname));
@@ -14,5 +98,6 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port);
-console.log("Weyy estamos en: " + port);
+server.listen(3000, () => {
+  console.log('Weyy estamos en: 3000');
+});
--- jsrps.js
+++ jsrps.js
@@ -9,145 +9,264 @@ const scissorsCPU = document.getElementById("scissorsImg2");
 const result = document.getElementById("result");
 const cpuScore = document.getElementById("cpuScore");
 const playerScore = document.getElementById("playerScore");
-var cpuTurn = Math.floor(Math.random() * 3);
+const newGameButton = document.getElementById("restart");
 var currentScorePlayer = 0;
 var currentScoreCPU = 0;
 
-function playerPlaysRock () {
+const defaultPlayer1Move = {
+    move: ""
+};
+
+const defaultPlayer2Move = {
+    move: ""
+};
+
+var socket = io();
+var player1Move = defaultPlayer1Move;
+var player2Move = defaultPlayer2Move;
+
+window.onload = function() {
+    requestNickname();
+};
+
+function requestNickname() {
+    let nickname = prompt("Please enter your nickname:", "");
+    if (nickname == null || nickname == "") {
+        alert("Nickname cannot be empty. Please try again.");
+        requestNickname(); // Prompt again if nickname is empty
+    } else {
+        socket
+        // Connect to the server and pass the nickname
+        socket.emit('userNickname', nickname);
+    }
+}
+
+socket.on('connectionAccepted', (message) => {
+    alert(message);
+    if (message === "You are the Player 1!!"){
+        rockCPU.style.pointerEvents = "none";
+        paperCPU.style.pointerEvents = "none";
+        scissorsCPU.style.pointerEvents = "none";
+    } else if (message === "You are the Player 2!!"){
+        paperPlayer.style.pointerEvents = "none";
+        scissorsPlayer.style.pointerEvents = "none";
+        rockPlayer.style.pointerEvents = "none";
+    }
+});
+
+socket.on('connectionRejected', (message) => {
+    if (message === 'Maximum users reached. Try again later.') {
+        alert(message);
+        // Perform the desired action, such as reloading the page
+        window.location.reload();
+    } else if (message === 'Nickname is already taken. Please choose a different one.') {
+        // Handle the case when the nickname is already taken
+        alert(message);
+        // Prompt for a new nickname, for example:
+        requestNickname();
+    }
+}); 
+
+function play () {
+    socket.emit('movePlayed', player1Move, player2Move);
+}
+
+function player1ClicksRock () {
+    player1Move.move = "rock";
+    play();
+}
+
+function player1PlaysRock () {
     paperPlayer.style.display = 'none';
     scissorsPlayer.style.display = 'none';
     trianglePlayer.style.display = 'none';
     rockPlayer.style.transform = "scale(2)";
     rockPlayer.style.transition = "transform 0.75s ease";
-    cpuPlay();
-    whoWon();
+    rockPlayer.style.pointerEvents = 'none';
 }
 
-function playerPlaysPaper () {
+function player1ClicksPaper () {
+    player1Move.move = "paper";
+    play();
+}
+
+function player1PlaysPaper () {
     rockPlayer.style.display = 'none';
     scissorsPlayer.style.display = 'none';
     trianglePlayer.style.display = 'none';
     paperPlayer.style.transform = "scale(2)";
     paperPlayer.style.transition = "transform 0.75s ease";
-    cpuPlay();
-    whoWon();
+    paperPlayer.style.pointerEvents = 'none';
 }
 
-function playerPlaysScissors () {
+function player1ClicksScissors () {
+    player1Move.move = "scissors";
+    play();
+}
+
+function player1PlaysScissors () {
     paperPlayer.style.display = 'none';
     rockPlayer.style.display = 'none';
     trianglePlayer.style.display = 'none';
     scissorsPlayer.style.transform = "scale(2)";
     scissorsPlayer.style.transition = "transform 0.75s ease";
-    cpuPlay();
-    whoWon();
+    scissorsPlayer.style.pointerEvents = 'none';
+}
+
+function player2ClicksRock () {
+    player2Move.move = "rock";
+    play();
+}
+
+function player2PlaysRock () {
+    paperCPU.style.display = 'none';
+    scissorsCPU.style.display = 'none';
+    triangleCPU.style.display = 'none';
+    rockCPU.style.transform = "scale(2)";
+    rockCPU.style.transition = "transform 0.75s ease";
+    rockCPU.style.pointerEvents = 'none';
+}
+
+function player2ClicksPaper () {
+    player2Move.move = "paper";
+    play();
+}
+
+function player2PlaysPaper () {
+    rockCPU.style.display = 'none';
+    scissorsCPU.style.display = 'none';
+    triangleCPU.style.display = 'none';
+    paperCPU.style.transform = "scale(2)";
+    paperCPU.style.transition = "transform 0.75s ease";
+    paperCPU.style.pointerEvents = 'none';
+}
+
+function player2ClicksScissors () {
+    player2Move.move = "scissors";
+    play();
+}
+
+function player2PlaysScissors () {
+    paperCPU.style.display = 'none';
+    rockCPU.style.display = 'none';
+    triangleCPU.style.display = 'none';
+    scissorsCPU.style.transform = "scale(2)";
+    scissorsCPU.style.transition = "transform 0.75s ease";
+    scissorsCPU.style.pointerEvents = 'none';
 }
 
 function restart () {
+    socket.emit('restart', "go");
+}
+
+socket.on('restarting', (message) => {
+    if (message === "go") {
+        restartGame();
+    }
+});
+
+function restartGame () {
     trianglePlayer.style.display = 'block';
     rockPlayer.style.display = 'block';
     rockPlayer.style.transform = "scale(1)";
     rockPlayer.style.transition = "transform 0.75s ease";
+    rockPlayer.style.pointerEvents = 'auto';
     paperPlayer.style.display = 'block';
     paperPlayer.style.transform = "scale(1)";
     paperPlayer.style.transition = "transform 0.75s ease";
+    paperPlayer.style.pointerEvents = 'auto';
     scissorsPlayer.style.display = 'block';
     scissorsPlayer.style.transform = "scale(1)";
     scissorsPlayer.style.transition = "transform 0.75s ease";
+    scissorsPlayer.style.pointerEvents = 'auto';
     triangleCPU.style.display = 'block';
     rockCPU.style.display = 'block';
     rockCPU.style.transform = "scale(1)";
     rockCPU.style.transition = "transform 0.75s ease";
+    rockCPU.style.pointerEvents = 'auto';
     paperCPU.style.display = 'block';
     paperCPU.style.transform = "scale(1)";
     paperCPU.style.transition = "transform 0.75s ease";
+    paperCPU.style.pointerEvents = 'auto';
     scissorsCPU.style.display = 'block';
     scissorsCPU.style.transform = "scale(1)";
     scissorsCPU.style.transition = "transform 0.75s ease";
+    scissorsCPU.style.pointerEvents = 'auto';
     result.innerHTML = ""; 
-    rockPlayer.style.pointerEvents = 'initial';
-    paperPlayer.style.pointerEvents = 'initial';
-    scissorsPlayer.style.pointerEvents = 'initial';
-}
-
-function cpuPlay () {
-
-    if (cpuTurn === 0){
-        paperCPU.style.display = 'none';
-        scissorsCPU.style.display = 'none';
-        triangleCPU.style.display = 'none';
-        rockCPU.style.transform = "scale(2)";
-        rockCPU.style.transition = "transform 0.75s ease";
-    }
-    
-    if (cpuTurn === 1) {
-        rockCPU.style.display = 'none';
-        scissorsCPU.style.display = 'none';
-        triangleCPU.style.display = 'none';
-        paperCPU.style.transform = "scale(2)";
-        paperCPU.style.transition = "transform 0.75s ease";
-    }
-    
-    if (cpuTurn === 2) {
-        paperCPU.style.display = 'none';
-        rockCPU.style.display = 'none';
-        triangleCPU.style.display = 'none';
-        scissorsCPU.style.transform = "scale(2)";
-        scissorsCPU.style.transition = "transform 0.75s ease";
-    }
-
+    player1Move.move = "";
+    player2Move.move = "";
+    newGameButton.style.pointerEvents = 'none';
+    console.log(player2Move.move + " " + player1Move.move);
 }
 
 function drawMessage () {
     result.style.transition = "transform 0.75s ease";
-    result.innerHTML = "It's a Draw!!";
-    cpuTurn = Math.floor(Math.random() * 3); 
+    result.innerHTML = "It's a Draw!!"; 
+    newGameButton.style.pointerEvents = 'auto';
+
 }
 
-function winMessage () {
+function win1Message () {
+    console.log(currentScorePlayer);
     result.style.transition = "transform 0.75s ease";
-    result.innerHTML = "You win!!"; 
+    result.innerHTML = "Player 1 wins!!"; 
     currentScorePlayer++;
     playerScore.innerHTML = currentScorePlayer; 
-    cpuTurn = Math.floor(Math.random() * 3);
+    newGameButton.style.pointerEvents = 'auto';
+
 }
 
-function loseMessage () {
+function win2Message () {
     result.style.transition = "transform 0.75s ease";
-    result.innerHTML = "You lose :/"; 
+    result.innerHTML = "Player 2 wins!!"; 
     currentScoreCPU++;
     cpuScore.innerHTML = currentScoreCPU; 
-    cpuTurn = Math.floor(Math.random() * 3);
+    newGameButton.style.pointerEvents = 'auto';
+
 }
 
-function whoWon () {
-
-    if ((cpuTurn === 0) && (rockPlayer.style.display === 'block')){
-        rockPlayer.style.pointerEvents = 'none';
-        drawMessage();
-    } else if ((cpuTurn === 1) && (paperPlayer.style.display === 'block')){
-        paperPlayer.style.pointerEvents = 'none';
-        drawMessage();
-    } else if ((cpuTurn === 2) && (scissorsPlayer.style.display === 'block')){
-        scissorsPlayer.style.pointerEvents = 'none';
-        drawMessage();
-    } else if ((cpuTurn === 0) && (paperPlayer.style.display === 'block')){
-        paperPlayer.style.pointerEvents = 'none';
-        winMessage();
-    } else if ((cpuTurn === 0) && (scissorsPlayer.style.display === 'block')){
-        scissorsPlayer.style.pointerEvents = 'none';
-        loseMessage();
-    } else if ((cpuTurn === 1) && (rockPlayer.style.display === 'block')){
-        rockPlayer.style.pointerEvents = 'none';
-        loseMessage();
-    } else if ((cpuTurn === 1) && (scissorsPlayer.style.display === 'block')){
-        scissorsPlayer.style.pointerEvents = 'none';
-        winMessage();
-    } else if ((cpuTurn === 2) && (rockPlayer.style.display === 'block')){
-        rockPlayer.style.pointerEvents = 'none';
-        winMessage();
-    } else if ((cpuTurn === 2) && (paperPlayer.style.display === 'block')){
-        paperPlayer.style.pointerEvents = 'none';
-        loseMessage();
+socket.on('move1Played', (msg) => {
+    player1Move.move = msg;
+    console.log("p1: " + msg);
+    if (msg === "rock") {
+        player1PlaysRock();
+    } else if (msg === "paper") {
+        player1PlaysPaper();
+    } else if (msg === "scissors") {
+        player1PlaysScissors();
     }
-}
+});
+
+socket.on('move2Played', (msg) => {
+    player2Move.move = msg;
+    console.log("p2: " + msg);
+    if (msg === "rock") {
+        player2PlaysRock();
+    } else if (msg === "paper") {
+        player2PlaysPaper();
+    } else if (msg === "scissors") {
+        player2PlaysScissors();
+    }
+});
+
+socket.on('whoWins', (message) => {
+    if (message === "Not connected") {
+        alert("Waiting for an opponent!");
+    } else if (message === "waiting") {
+        result.innerHTML = "Waiting for opponent's move..."; 
+    } else if (message === "draw") {
+        // Draw or same move
+        console.log("draw");
+        drawMessage();
+    } else if (message === "player1") {
+        // Player 1 wins
+        console.log("p1");
+        win1Message();
+    } else if (message === "player2") {
+        console.log("p2");
+        // Player 2 wins
+        win2Message();
+    } else {
+        console.log("f");
+    }
+});

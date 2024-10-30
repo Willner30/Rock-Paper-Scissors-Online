42482f944c8acbcb75cc6f4a13a23f0b039d9f60
Wilfredo Mota <willmdj@hotmail.com>
2024-10-30 19:17:28 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -7,12 +7,17 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const { emit } = require('process');
 const io = new Server(server);
+
+
 let connectedUsers = {
   playerOne: "",
   playerTwo: ""
 };
-let p1Move = "";
-let p2Move = "";
+const playersMove = {
+  p1Movement: "",
+  p2Movement: ""
+};
+
 
 io.on('connection', (socket) => {
   // Handle user nickname validation
@@ -52,39 +57,59 @@ io.on('connection', (socket) => {
   });
   socket.on('movePlayed', (player1Move, player2Move) => {  //Verifies moves on the server side
 
-    p1Move = player1Move.move;
-    p2Move = player2Move.move;
+    let moves = {
+      p1Move: player1Move.move,
+      p2Move: player2Move.move
+    };
 
-    console.log(p1Move);
-    io.emit('move1Played', p1Move);
-    console.log(p2Move);
-    io.emit('move2Played', p2Move);
+    if (moves.p1Move !== ""){
+      playersMove.p1Movement = moves.p1Move;
+    };
+    if (moves.p2Move !== ""){
+      playersMove.p2Movement = moves.p2Move
+    };
+
     function whoWon() {
       if (connectedUsers.playerOne === "" || connectedUsers.playerTwo === "") {
         socket.emit('whoWins', 'Not connected');
-      } else if (p1Move === "" || p2Move === "") {
-        console.log(p1Move + " y " + p2Move);
+      } else if (playersMove.p1Movement === "" || playersMove.p2Movement === "") {
         socket.emit('whoWins', 'waiting');
-      } else if (p1Move === p2Move) {
-        // Draw or same move
-        io.emit('whoWins', 'draw');
-      } else if (
-        (p1Move === "rock" && p2Move === "scissors") ||
-        (p1Move === "paper" && p2Move === "rock") ||
-        (p1Move === "scissors" && p2Move === "paper")
-        ) {
-          // Player 1 wins
-        io.emit('whoWins', 'player1');
+        console.log("waiting...");
       } else {
-        // Player 2 wins
-        io.emit('whoWins', 'player2');
-      }
+        console.log(playersMove);
+        io.emit('move1Played', playersMove.p1Movement);
+        io.emit('move2Played', playersMove.p2Movement);
+        console.log(playersMove);
+        if (playersMove.p1Movement === playersMove.p2Movement) {
+          // Draw or same move
+          io.emit('whoWins', 'draw');
+          playersMove.p1Movement = ""; 
+          playersMove.p2Movement = "";
+        } else if (
+          (playersMove.p1Movement === "rock" && playersMove.p2Movement === "scissors") ||
+          (playersMove.p1Movement === "paper" && playersMove.p2Movement === "rock") ||
+          (playersMove.p1Movement === "scissors" && playersMove.p2Movement === "paper")
+          ) {
+            // Player 1 wins
+          io.emit('whoWins', 'player1');
+          playersMove.p1Movement = ""; 
+          playersMove.p2Movement = "";
+        } else {
+          // Player 2 wins
+          io.emit('whoWins', 'player2');
+          playersMove.p1Movement = ""; 
+          playersMove.p2Movement = "";
+        }
+      } 
     }  
+
     whoWon();
 
   });
   socket.on('restart', (msg) => {
     if (msg === "go") {
+      playersMove.p1Movement = ""; 
+      playersMove.p2Movement = "";
       io.emit('restarting', msg);
     }
   });
